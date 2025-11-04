c9afa3158103913c781fb1884e6de3bcea92dc63
rajavirat <rajaviratshukla@gmail.com>
2025-11-04 15:36:19 +0530

    Made some changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,27 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD5nQb7jXjFVv8ezICMIQhZbJDWtqjxbr0",
-  authDomain: "time-table-generator-c1e95.firebaseapp.com",
-  databaseURL: "https://time-table-generator-c1e95-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "time-table-generator-c1e95",
-  storageBucket: "time-table-generator-c1e95.firebasestorage.app",
-  messagingSenderId: "261181239373",
-  appId: "1:261181239373:web:bf54ac52fd26b5bbac6c04",
-  measurementId: "G-GKFWNLK610"
+  apiKey: "AIzaSyA5My9rrC8WdXurddCOug6-rMOKiPw7F_U", // You need to add your actual API key
+  authDomain: "time-table-generator-5c5a2.firebaseapp.com",
+  projectId: "time-table-generator-5c5a2",
+  storageBucket: "time-table-generator-5c5a2.firebasestorage.app",
+  messagingSenderId: "1013597823425",
+  appId: "1:1013597823425:web:5b348ad798e53cf17f8f40",
+  measurementId: "G-GGM02HT77X"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize services
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Export for use in other files
+export { app, analytics, db, auth };
